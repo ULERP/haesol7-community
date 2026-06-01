e21235c2c3dd9d7b6375e506ff63ef9bcb425063
ULERP <halos80@naver.com>
2026-06-01 11:23:49 +0000

    fix: MEDIA_URL 절대경로 수정 + 모바일 이미지 서빙 항상 활성화

--- hello_world/settings.py
+++ hello_world/settings.py
@@ -93,7 +93,7 @@ STATIC_URL = "static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_DIRS = [BASE_DIR / "hello_world" / "static"]
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
-MEDIA_URL = "media/"
+MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "hello_world" / "media"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
--- hello_world/urls.py
+++ hello_world/urls.py
@@ -9,8 +9,10 @@ urlpatterns = [
     path('__reload__/', include('django_browser_reload.urls')),
 ]
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+# media 파일은 항상 서빙 (PythonAnywhere 포함)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 handler404 = 'hello_world.core.views.error_404'
 handler500 = 'hello_world.core.views.error_500'
