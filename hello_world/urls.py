dadc3735c024b3fd3c92a98e610c7506e8292783
ULERP <halos80@naver.com>
2026-06-01 13:03:53 +0000

    fix: media 서빙 DEBUG 전용으로 복구 (PythonAnywhere Static files로 처리)

--- hello_world/urls.py
+++ hello_world/urls.py
@@ -10,9 +10,7 @@ urlpatterns = [
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
-# media 파일은 항상 서빙 (PythonAnywhere 포함)
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 handler404 = 'hello_world.core.views.error_404'
 handler500 = 'hello_world.core.views.error_500'
