c495be3b6ecb18bfac7265115e22cf6f7c9d4dd3
ULERP <halos80@naver.com>
2026-06-03 15:14:17 +0000

    fix: navbar 기록하기 메뉴 DB 게시판과 동적 매칭

--- hello_world/settings.py
+++ hello_world/settings.py
@@ -65,7 +65,6 @@ TEMPLATES = [
                 "django.contrib.messages.context_processors.messages",
                 "hello_world.context_processors.active_boards",
                 "hello_world.context_processors.verification_status",
-                "hello_world.core.context_processors.sidebar_data",
                 "hello_world.core.context_processors.boards_context",
             ],
         },
