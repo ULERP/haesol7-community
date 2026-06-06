0a55643fddb78c271ef1fa8f1374ea7fc01c08a8
ULERP <halos80@naver.com>
2026-06-06 14:36:18 +0000

    fix: base_2panel 사이드바 navbar와 일치, sb_boards trade/qna 제외

--- hello_world/core/context_processors.py
+++ hello_world/core/context_processors.py
@@ -2,7 +2,7 @@ from .models import Board
 
 def boards_context(request):
     """사이드바용 게시판 목록"""
-    boards = Board.objects.filter(is_active=True).order_by('order', 'id')
+    boards = Board.objects.filter(is_active=True).exclude(board_type__in=['trade','qna','gallery']).order_by('order', 'id')
     return {
         'sb_boards': boards,
     }
