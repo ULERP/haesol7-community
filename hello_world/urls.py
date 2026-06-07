b6272b78a26b0be9b2b8291e4e2540e3e197ede0
ULERP <halos80@naver.com>
2026-06-07 12:07:54 +0000

    feat: favicon.ico 추가

--- hello_world/urls.py
+++ hello_world/urls.py
@@ -2,7 +2,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic.base import RedirectView
+from django.contrib.staticfiles.storage import staticfiles_storage
+
 urlpatterns = [
+    path('favicon.ico', RedirectView.as_view(url=staticfiles_storage.url('favicon.ico'), permanent=True)),
     path('', include('hello_world.core.urls')),
     path('admin/', admin.site.urls),
     path('community/', include('community.urls')),
